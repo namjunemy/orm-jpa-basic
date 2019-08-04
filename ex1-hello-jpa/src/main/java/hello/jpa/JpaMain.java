389013fc49874c57d30b2a113184001a75d87f6b
Namjun Kim <namjunemy@gmail.com>
2019-08-05 02:40:24 +0900

    jpql 실습. 엔티티를 대상으로 쿼리, 페이징

--- ex1-hello-jpa/src/main/java/hello/jpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hello/jpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -15,10 +16,10 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setId(2L);
-            member.setName("NJ");
-            em.persist(member);
+            List<Member> members = em.createQuery("select m from Member as m", Member.class)
+                .setFirstResult(5)
+                .setMaxResults(5)
+                .getResultList();
 
             tx.commit();
         } catch (Exception e) {
