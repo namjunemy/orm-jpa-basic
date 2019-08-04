d991402dced2c0a078f979c8e6d6a72c000b83cb
Namjun Kim <namjunemy@gmail.com>
2019-08-05 01:23:26 +0900

    EntityManagerFactory, EntityManager setup, close test

---
+++ ex1-hello-jpa/src/main/java/hello/jpa/JpaMain.java
@@ -0,0 +1,17 @@
+package hello.jpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        em.close();
+
+        emf.close();
+    }
+}
