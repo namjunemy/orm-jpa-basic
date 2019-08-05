d1d43c6f0c981569f9500abf8e83b70780b45057
김남준 <njkim@zuminternet.com>
2019-08-05 19:08:20 +0900

    영속성 컨텍스트의 1차 캐시에서 조회하기, SELECT 쿼리 안함

--- ex1-hello-jpa/src/main/java/hello/jpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hello/jpa/JpaMain.java
@@ -4,9 +4,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
+
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
@@ -16,10 +16,21 @@ public class JpaMain {
         tx.begin();
 
         try {
-            List<Member> members = em.createQuery("select m from Member as m", Member.class)
-                .setFirstResult(5)
-                .setMaxResults(5)
-                .getResultList();
+            // 비영속
+            Member member = new Member();
+            member.setId(104L);
+            member.setName("NJ");
+
+            // 영속
+            System.out.println("--- BEFORE ---");
+            em.persist(member);
+            System.out.println("--- AFTER ---");
+
+            // DB에 셀렉트 쿼리가 나가지 않는다. 영속성 컨텍스트의 1차 캐시에 저장되어 있기 때문에 캐시에서 바로 가져온다.
+            Member findMember = em.find(Member.class, 104L);
+
+            System.out.println("findMember.id = " + findMember.getId());
+            System.out.println("findMember.name = " + findMember.getName());
 
             tx.commit();
         } catch (Exception e) {
