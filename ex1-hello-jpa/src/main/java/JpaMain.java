ab0889cf17cad9ba514cd1af57548ef07bfef16d
Namjun Kim <namjunemy@gmail.com>
2019-08-28 19:01:11 +0900

    고아 객체 제거 orphanRemoval 옵션

--- ex1-hello-jpa/src/main/java/JpaMain.java
+++ ex1-hello-jpa/src/main/java/JpaMain.java
@@ -29,6 +29,12 @@ public class JpaMain {
 
             em.persist(parent);
 
+            em.flush();
+            em.clear();
+
+            Parent findParent = em.find(Parent.class, parent.getId());
+            em.remove(findParent);
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- ex1-hello-jpa/src/main/java/cascade/Parent.java
+++ ex1-hello-jpa/src/main/java/cascade/Parent.java
@@ -22,7 +22,7 @@ public class Parent {
 
     private String name;
 
-    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Child> children = new ArrayList<>();
 
     public void addChild(Child child) {
