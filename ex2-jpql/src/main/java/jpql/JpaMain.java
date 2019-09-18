9fc4e25360af1deecf4c7df3029409c6b1098e65
Namjun Kim <namjunemy@gmail.com>
2019-09-18 21:05:49 +0900

    jpql TypeQuery, Query

--- ex2-jpql/src/main/java/jpql/JpaMain.java
+++ ex2-jpql/src/main/java/jpql/JpaMain.java
@@ -4,6 +4,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 import jpql.domain.Member;
 
 public class JpaMain {
@@ -20,7 +22,15 @@ public class JpaMain {
 
             Member member = new Member();
             member.setName("nj1");
+            member.setAge(10);
             em.persist(member);
+
+            TypedQuery<Member> query =
+                em.createQuery("select m from Member m", Member.class);
+            TypedQuery<String> query2 =
+                em.createQuery("select m.name from Member m", String.class);
+            Query query3 =
+                em.createQuery("select m.name, m.age from Member m");
 
             tx.commit();
         } catch (Exception e) {
