a672d1ec090ac53de18450f3bad017c29bddb260
Namjun Kim <namjunemy@gmail.com>
2019-09-24 01:06:09 +0900

    JPQL 컬렉션 값 연관 경로 표현식 - from 절 별칭 사용시 탐색 가능

--- ex2-jpql/src/main/java/jpql/JpaMain.java
+++ ex2-jpql/src/main/java/jpql/JpaMain.java
@@ -8,6 +8,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.Collection;
 import java.util.List;
 
 public class JpaMain {
@@ -32,13 +33,20 @@ public class JpaMain {
             member.changeTeam(team);
             em.persist(member);
 
+            Member member2 = new Member();
+            member2.setName("관리자2");
+            member2.setAge(27);
+            member2.setMemberType(MemberType.ADMIN);
+            member2.changeTeam(team);
+            em.persist(member2);
+
             em.flush();
             em.clear();
 
             String query =
-                "select m.team from Member m";
+                "select m.name from Team t join t.members m";
 
-            List<Team> result = em.createQuery(query, Team.class)
+            List<String> result = em.createQuery(query, String.class)
                 .getResultList();
 
             System.out.println(result);
