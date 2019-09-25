bc3fdc3608bb4b1540641ed221fbf6a7dd3789ff
Namjun Kim <namjunemy@gmail.com>
2019-09-26 00:43:22 +0900

    JPQL fetch join으로 SELECT N+1 문제 해결

--- ex2-jpql/src/main/java/jpql/JpaMain.java
+++ ex2-jpql/src/main/java/jpql/JpaMain.java
@@ -47,7 +47,7 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select m from Member m";
+            String query = "select m from Member m join fetch m.team";
 
             List<Member> result = em.createQuery(query, Member.class)
                 .getResultList();
